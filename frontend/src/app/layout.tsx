e4cbbe3eb86976eff8ed8bbfb77050eb7a6874f0
Ashish Jangde <ashishjangde2003@gmail.com>
2026-01-09 19:30:32 +0530

    added favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,9 +16,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "GroqChat",
   description: "Chat with LLM",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({
